a6ed5e22d0145cbfd6ae1f3a877429242358ff9f
Daniel <daniicc33@gmail.com>
2025-03-26 21:54:59 -0500

    petstore database

---
+++ src/config/database.php
@@ -0,0 +1,16 @@
+<?php
+    $host      = "localhost";
+    $porr      = "5432";
+    $dbname    = "cm";
+    $user      = "";
+    $password  = "";
+
+    $conn = "
+        host=$host
+        port=$port
+        dbname=$dbname
+        user=$user
+        password=$password
+    ";
+    
+?>
