eb4f7f257692bef7ede58fe26ef52204f5592efc
Daniel <daniicc33@gmail,com>
2025-03-27 20:06:08 -0500

    database petstore

--- src/config/database.php
+++ src/config/database.php
@@ -1,16 +1,23 @@
 <?php
-    $host      = "localhost";
-    $porr      = "5432";
-    $dbname    = "cm";
-    $user      = "";
-    $password  = "";
-
-    $conn = "
-        host=$host
-        port=$port
-        dbname=$dbname
-        user=$user
-        password=$password
-    ";
+    $host      ="localhost";
+    $port      ="5432  ";
+    $dbname    ="petstore";
+    $user      ="postgres";
+    $password  ="unicesmag";  
     
+    $data_connection ="
+    host=$host
+    port=$port
+    dbname=$dbname
+    user=$user
+    password=$password
+    ";
+    $conn = pg_connect($data_connection);
+    if(!$conn){
+        echo "conection error";
+    }else{
+        echo "conection success!!!";
+    }
+    pg_close($conn);
+
 ?>
